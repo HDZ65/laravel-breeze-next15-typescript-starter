46bcdb779d226816a4d5f70f4ffdcd5a1b743ef4
Alexandre <alexandre.hernandez@yahoo.com>
2025-01-13 18:47:38 +0100

    d

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,4 +6,14 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/env-check', function() {
+    return [
+        'app_url' => env('APP_URL'),
+        'db_host' => env('DB_HOST'),
+        'db_name' => env('DB_DATABASE'),
+        'db_connection' => env('DB_CONNECTION'),
+        'app_env' => env('APP_ENV')
+    ];
+});
+
 require __DIR__.'/auth.php';
